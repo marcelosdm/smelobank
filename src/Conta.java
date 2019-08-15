c16d221a53dbe93b3bc37d02aaf11b2a770cbc7f
marcelosdm <marcelosdm@gmail.com>
2019-08-15 08:39:58 -0300

    Add methods do Conta.java and TestaMetodos.java

--- src/Conta.java
+++ src/Conta.java
@@ -7,4 +7,26 @@ public class Conta {
   int agencia;
   int numero;
   String titular;
+
+  public double deposita(double valor) {
+    this.saldo += valor;
+    return valor;
+  }
+
+  public boolean saca(double valor) {
+    if (this.saldo >= valor) {
+      this.saldo -= valor;
+      return true;
+    }
+    return false;
+  }
+
+  public boolean transfere(double valor, Conta destino) {
+    if (this.saldo >= valor) {
+      this.saldo -= valor;
+      destino.deposita(valor);
+      return true;
+    }
+    return false;
+  }
 }
